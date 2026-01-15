147cc1711d1a40c3f92750815ea3f7084f99ec8a
kazuki1023 <k.keio12569@gmail.com>
2026-01-15 21:02:44 +0900

    fix: :fire: deployにあたり、vercelでは対応できない箇所を削除

--- mastra/src/mastra/agents/weather-agent.ts
+++ mastra/src/mastra/agents/weather-agent.ts
@@ -1,8 +1,6 @@
 import { Agent } from '@mastra/core/agent';
-import { Memory } from '@mastra/memory';
-import { LibSQLStore } from '@mastra/libsql';
-import { weatherTool } from '../tools/weather-tool';
 import { scorers } from '../scorers/weather-scorer';
+import { weatherTool } from '../tools/weather-tool';
 
 export const weatherAgent = new Agent({
   name: 'Weather Agent',
@@ -45,9 +43,9 @@ export const weatherAgent = new Agent({
       },
     },
   },
-  memory: new Memory({
-    storage: new LibSQLStore({
-      url: 'file:../mastra.db', // path is relative to the .mastra/output directory
-    }),
-  }),
+  // memory: new Memory({
+  //   storage: new LibSQLStore({
+  //     url: 'file:../mastra.db', // path is relative to the .mastra/output directory
+  //   }),
+  // }),
 });
--- mastra/src/mastra/index.ts
+++ mastra/src/mastra/index.ts
@@ -1,6 +1,6 @@
 
 import { Mastra } from '@mastra/core/mastra';
-import { LibSQLStore } from '@mastra/libsql';
+// import { LibSQLStore } from '@mastra/libsql';
 import { PinoLogger } from '@mastra/loggers';
 import { majiangAnalysisAgent } from './agents/majiang-analysis-agent';
 import { weatherAgent } from './agents/weather-agent';
@@ -12,10 +12,11 @@ export const mastra = new Mastra({
   workflows: { weatherWorkflow, evaluateShoupaiWorkflow },
   agents: { weatherAgent, majiangAnalysisAgent },
   scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
-  storage: new LibSQLStore({
-    // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
+  // vercel storage is not supported yet
+  // storage: new LibSQLStore({
+  //   // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
+  //   url: ":memory:",
+  // }),
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
